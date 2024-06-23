86b4c6e6181b9f67f1920dfc2cf3aa68e8c3311e
vic-bart <vicbart08@gmail.com>
2024-06-23 21:15:44 +1000

    Finished selection sort doc.

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,56 +1,61 @@
 """
 Hi! This file defines bubble sort for arrays, a stable sorting algorithm that works by traversing an array from the end, to the start where it iteratively accumulates the minimum unsorted values. 
-Here's an example:
+  - Here's an example:
 
-  >>  array = [3, 0, 2, 9, 4, 7, 6, 5, 8, 1]
+    >>  array = [3, 0, 2, 9, 4, 7, 6, 5, 8, 1]
 
-  - To find the minimum unsorted value, bubble sort compares immediately adjacent values, shifting the smaller value towards the start.
+    - To find the minimum unsorted value, bubble sort compares immediately adjacent values, shifting the smaller value towards the start.
 
-  for i in range(n):
-  
-    for j in range(n-1...i):
+    for i in range(n):
     
-        if array[j-1] > array[j]:
-        
-          swap(array, j-1, j)
+      for j in range(n-1...i):
+      
+          if array[j-1] > array[j]:
+          
+            swap(array, j-1, j)
 
-  - See what happens when the current value is smaller than the next value.
+    - See what happens when the current value is smaller than the next value.
 
-  >>  array = [3, 0, 2, 9, 4, 7, 6, 5, 8, 1]
-  
-  i = 0, j = 9
-  array[8] > array[9] == True (i.e. 8 > 1 == True)
-  swap array[8] with array[9]
+    >>  array = [3, 0, 2, 9, 4, 7, 6, 5, 8, 1]
+    
+    i = 0, j = 9
+    array[8] > array[9] == True (i.e. 8 > 1 == True)
+    swap array[8] with array[9]
 
-  >>  array = [3, 0, 2, 9, 4, 7, 6, 5, 1, 8]
+    >>  array = [3, 0, 2, 9, 4, 7, 6, 5, 1, 8]
 
-  - See what happens when the current value is greater than or equal to the next value.
+    - See what happens when the current value is greater than or equal to the next value.
 
-  >>  array = [3, 0, 1, 2, 9, 4, 7, 6, 5, 8]
+    >>  array = [3, 0, 1, 2, 9, 4, 7, 6, 5, 8]
 
-  i = 0, j = 2
-  array[1] > array[2] == False (i.e. 0 > 1 == False)
-  do nothing
+    i = 0, j = 2
+    array[1] > array[2] == False (i.e. 0 > 1 == False)
+    do nothing
 
-  >>  array = [3, 0, 1, 2, 9, 4, 7, 6, 5, 8]
+    >>  array = [3, 0, 1, 2, 9, 4, 7, 6, 5, 8]
 
-  - This repeats all the way until the end of the list is reached. 
+    - This repeats all the way until the end of the list is reached. 
 
-  >>  array = [0, 3, 1, 2, 9, 4, 7, 6, 5, 8]
+    >>  array = [0, 3, 1, 2, 9, 4, 7, 6, 5, 8]
 
-  - Notice that the array as a whole is not sorted, but the minimum unsorted value is in its sorted position (i.e. if the array were sorted, the value would have the same position). 
-  - In fact, this is an invariant; at the end of each loop, the minimum unsorted value will always be moved to its sorted position. 
-  - And since the values are sorted in ascending order, the array is partitioned in two -- the lower part contains sorted values, the upper part contains unsorted values. Each loop moves the minimum value from its unsorted position in the upper part, to its sorted position in the lower part, extending the sorted partition until it includes the whole array.
+    - Notice that the array as a whole is not sorted, but the minimum unsorted value is in its sorted position (i.e. if the array were sorted, the value would have the same position). 
+
+    - In fact, this is an invariant; at the end of each loop, the minimum unsorted value will always be moved to its sorted position. 
+
+    - And since the values are sorted in ascending order, the array is partitioned in two -- the lower part contains sorted values, the upper part contains unsorted values. Each loop moves the minimum value from its unsorted position in the upper part, to its sorted position in the lower part, extending the sorted partition until it includes the whole array.
 
 The best-case time complexity is O(n):
 
   - Recall the invariant -- at the end of each loop, the minimum unsorted value will always be moved to its sorted position. But what if at the end of the loop, no values have been moved? 
+
   - Recall that bubble sort compares pairs of immediately adjacent values and swaps them if they're in descending order, but if no values have been moved, than each pair of immediately adjacent values must be in ascending order, and thus the whole array must be in ascending order, and thus is sorted. 
+
   - This provides the algorithm with an early exit; if no values have been swapped, the array is sorted and the algorithm terminates. But even if the input array was sorted, the algorithm can only determine it by iterating over the whole array once, and thus the best-case time complexity is O(n).
 
 The average/worst-case time complexity is O(n^2):
 
   - Note that because the lower part will always be made of values smaller than all values in the upper part, the sorted position of the upper part's minimum value is always immediately adjacent to the lower part (because this position is greater than all the sorted values in the lower part, and smaller than all the unsorted values in the upper part).
+  
   - This means we never need to iterate over the sorted values; we only iterate over the unsorted values, which decreases by one each loop. Defining 'n' as the number of values for a given array (i.e. the length of the array):
 
   (n-1) + (n-2) + ... + 2 + 1 + c
--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,48 +1,83 @@
 """
-Hi! This file defines selection sort for arrays, an unstable sorting algorithm that works by partitioning an array in two, where the lower sorted half iteratively accumulates the minimum values of the upper unsorted 
-half. 
-Note that this is very similar to bubble sort, the main difference being we only swap the desired value after iterating over all other unsorted values (bubble sort swaps the desired value while iterating over all 
-other unsorted values). 
-Here's an example:
+Hi! This file defines selection sort for arrays, an unstable sorting algorithm that works by partitioning an array in two, where the lower sorted part iteratively accumulates the minimum values of the upper unsorted part. 
 
-  >>  array = [3, 2, 1, 9, 4, 7, 6, 5, 8, 0]
+  - Recall bubble sort, which sorts an array by swapping the minimum unsorted values into their sorted positions. Selection sort works on the same principle, but attempts to sort more efficiently by minimising the number of swaps as much as possible; rather than incrementally swapping the minimum unsorted value towards its sorted position, selection sort iterates over all unsorted values, finds the minimum unsorted value, and swaps it directly into its sorted position.
 
-  - To find the minimum unsorted value, selection sort compares the current minimum unsorted value -- which is initialised to the unsorted value immediately adjacent to the sorted partition -- with every other 
-  unsorted value. If a lower value is found, it replaces the current minimum unsorted value. After all unsorted values have been compared, the current minimum unsorted value is guaranteed to be the minimum unsorted 
-  value, and is swapped with the unsorted value immediately adjacent to the sorted partition.
+  - Here's an example:
 
-  min = i
-  for j in (i+1...n):
-    if array[j] < array[min]:
-      min = j
-  swap(array, i, min)
+    >>  array = [3, 2, 1, 9, 4, 7, 6, 5, 8, 0]
 
-  - See what happens when an unsorted value is less than the first unsorted value.
+    - To find the minimum unsorted value, selection sort compares the current minimum unsorted value -- which is initialised to the unsorted value immediately adjacent to the sorted partition -- with every other unsorted value. If a lower value is found, it replaces the current minimum unsorted value. After all unsorted values have been compared, the current minimum unsorted value is guaranteed to be the minimum unsorted value, and is swapped with the unsorted value immediately adjacent to the sorted partition.
 
-  >>  array = [3, 2, 1, 9, 4, 7, 6, 5, 8, 0]
+    min = i
 
-  j = 1, min = 0
-  array[j] < array[min] == True (2 < 3 == True)
-  min = 1
+    for j in (i+1...n):
+    
+      if array[j] < array[min]:
+      
+        min = j
+        
+    swap(array, i, min)
 
-  >>  array = [3, 2, 1, 9, 4, 7, 6, 5, 8, 0]
+    - See what happens when an unsorted value is less than the first unsorted value.
 
-  - Note that no values have been swapped. See what happens when the last unsorted value is compared (Note that at this point, the sorted partition is empty, and the unsorted partition includes the whole array).
+    >>  array = [3, 2, 1, 9, 4, 7, 6, 5, 8, 0]
 
-  >>  array = [3, 2, 1, 9, 4, 7, 6, 5, 8, 0]
+    j = 1, min = 0
+    array[j] < array[min] == True (2 < 3 == True)
+    min = 1
 
-  j = n-1, min = 2, i = 0
-  array[j] < array[min] == True (0 < 1 == True)
-  min = n-1
-  j += 1, j == n (exit loop)
-  swap array[i] with array[min]
+    >>  array = [3, 2, 1, 9, 4, 7, 6, 5, 8, 0]
 
-  >>  array = [0, 2, 1, 9, 4, 7, 6, 5, 8, 3]
+    - Note that no values have been swapped. See what happens when the last unsorted value is compared (Note that at this point, the sorted partition is empty, and the unsorted partition includes the whole array).
 
-  - Notice that the array as a whole is not sorted, but the minimum unsorted value is in its sorted position (i.e. if the array were sorted, the value would have the same position). 
-  - In fact, this is an invariant; at the end of each loop, the minimum unsorted value will always be moved to its sorted position. 
-  - And since the values are sorted in ascending order, the array is partitioned in two -- the lower half contains sorted values, the upper half contains unsorted values. Each loop moves the minimum value from its 
-  unsorted position in the upper half, to its sorted position in the lower half, extending the sorted partition until it includes the whole array.
+    >>  array = [3, 2, 1, 9, 4, 7, 6, 5, 8, 0]
+
+    j = n-1, min = 2, i = 0
+    array[j] < array[min] == True (0 < 1 == True)
+    min = n-1
+    j += 1, j == n (exit loop)
+    swap array[i] with array[min]
+
+    >>  array = [0, 2, 1, 9, 4, 7, 6, 5, 8, 3]
+
+    - Notice that the array as a whole is not sorted, but the minimum unsorted value is in its sorted position (i.e. if the array were sorted, the value would have the same position). 
+
+    - In fact, this is an invariant; at the end of each loop, the minimum unsorted value will always be moved to its sorted position. 
+
+    - And since the values are sorted in ascending order, the array is partitioned in two -- the lower part contains sorted values, the upper part contains unsorted values. Each loop moves the minimum value from its unsorted position in the upper part, to its sorted position in the lower part, extending the sorted partition until it includes the whole array.
+
+  - Recall that selection sort attempts to improve upon bubble sort by minimising swaps. But does it succeed? Yes, selection sort is generally more efficient because of fewer swaps being performed, but the time complexity (found below) has not improved. In fact, the best-case time complexity has worsened to O(n^2) (previously O(n)):
+
+    - In bubble sort, the swapping of immediately adjacent values revealed information about the whole array -- if swaps were performed, there existed a pair of immediately adjacent values that were not in ascending order, and thus the array could not be in ascending order (i.e. not sorted); if no swaps were performed, each pair must be in ascending order, and thus the whole array must be in ascending order (i.e. sorted).
+
+    - In selection sort, no information is revealed about the array as a whole -- the algorithm only identifies the minimum unsorted value, and swaps it into its sorted position, which is already known without incremental swapping (this position is immediately adjacent to the sorted partition, where it is greater than all the sorted values in the lower part, and smaller than all the unsorted values in the upper part).
+
+    - Thus, selection sort cannot identify when the whole array has been sorted, and must always iterate over the whole unsorted partition.
+
+  - Additionally, the removal of incremental swapping also makes the algorithm unstable:
+
+    - When the minimum unsorted value is swapped into its sorted position, the original value which existed there prior, is moved into the minimum unsorted value's vacated position.
+
+    - If other values equal to this original value, were previously positioned after the original value, and are now positioned before it, their sorted positions may come before the original value's sorted position, making the algorithm unstable.
+
+    - Here's an example (consider 7a and 7b as being equivalent to 7; the letters just make it easier to differentiate them):
+
+               >>  array = [1, 2, 3, 7a, 7b, 6, 5, 4]
+        sorted partition = [1, 2, 3]
+      unsorted partition =          [7a, 7b, 6, 5, 4]
+
+      - The minimum unsorted value is 4, and it's sorted position is immediately adjacent to the sorted partition.
+
+               >>  array = [1, 2, 3, 4, 7b, 6, 5, 7a]
+        sorted partition = [1, 2, 3, 4]
+      unsorted partition =             [7b, 6, 5, 7a]
+
+      - This is the moment where the sorted order of equivalent values is violated; 7a must come before 7b in the sorted array, as it was in the unsorted array. However, this violation will persist.
+
+               >>  array = [1, 2, 3, 4, 5, 6, 7b, 7a] ([7b, 7a] = unstable, [7a, 7b] = stable)
+        sorted partition = [1, 2, 3, 4, 5, 6, 7b, 7a]
+      unsorted partition =
 
 The best/average/worst-case time complexity is O(n^2):
 
@@ -73,7 +108,7 @@ The best/average/worst-case space complexity is O(1). Here's an explanation:
 
 def selection_sort(array:list) -> None:
   """
-  An unstable sorting algorithm that works by partitioning an array in two, where the lower sorted half iteratively accumulates the minimum values of the upper unsorted half. 
+  An unstable sorting algorithm that works by partitioning an array in two, where the lower sorted part iteratively accumulates the minimum values of the upper unsorted part.
 
   Input:
     - array (list)
